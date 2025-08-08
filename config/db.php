b2d115c00125e15b9a3facb9632959f84b278a4e
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-08-08 14:33:49 -0600

    no adjunte este en  el push jsjs

---
+++ config/db.php
@@ -0,0 +1,17 @@
+<?php
+class Database {
+    public static function connect() {
+        $host = 'db'; 
+        $dbname = 'idk_ambiente_web';
+        $username = 'usuario';
+        $password = 'clave';
+
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+}
